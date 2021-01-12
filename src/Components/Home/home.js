f9604293488756972d960059c8a881c15a231027
abhishek2000123 <43308804+abhishek2000123@users.noreply.github.com>
2021-01-13 02:50:47 +0530

    transition fixed

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -5,53 +5,68 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import logo from "../images/noun_Cricket_2369453.svg";
 import Register from "./register";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 class home extends React.Component {
   render() {
     return (
-      <div
-        className="login"
-        style={{
-          backgroundImage: `url(${Homebg})`,
+      <div>
+        <div
+          className="login"
+          style={{
+            backgroundImage: `url(${Homebg})`,
 
-          height: "800px",
-          opacity: 0.8
-        }}
-      >
-        <div class="Jumbotron">
-          <img src={logo} />
-          <div class="row g-3 align-items-center">
-            <label for="inputPassword6" class="col-form-label">
-              Username
-            </label>
+            height: "800px",
+            opacity: 0.8
+          }}
+        >
+          <div class="Jumbotron">
+            <img src={logo} />
+            <div class="row g-3 align-items-center">
+              <label for="inputPassword6" class="col-form-label">
+                Username
+              </label>
 
-            <input
-              type="text"
-              id="inputPassword6"
-              class="form-control"
-              aria-describedby="passwordHelpInline"
-            />
+              <input
+                type="text"
+                id="inputPassword6"
+                class="form-control"
+                aria-describedby="passwordHelpInline"
+              />
+            </div>
+            <div class="row g-3 align-items-center">
+              <label for="inputPassword6" class="col-form-label">
+                Password
+              </label>
+
+              <input
+                type="password"
+                id="inputPassword6"
+                class="form-control"
+                aria-describedby="passwordHelpInline"
+              />
+            </div>
+            <br />
+            <button type="submit" class="btn btn-dark btn-block">
+              <Link to="/" className="linkto">
+                Submit
+              </Link>
+            </button>
+            <br />
+
+            <button type="submit" class="btn btn-dark btn-block">
+              <Link
+                to={{
+                  pathname: "/Register",
+                  state: { prev: false }
+                }}
+                className="linkto"
+              >
+                {/* {" "} */}
+                Register
+              </Link>
+            </button>
           </div>
-          <div class="row g-3 align-items-center">
-            <label for="inputPassword6" class="col-form-label">
-              Password
-            </label>
-
-            <input
-              type="password"
-              id="inputPassword6"
-              class="form-control"
-              aria-describedby="passwordHelpInline"
-            />
-          </div>
-          <br />
-          <button type="submit" class="btn btn-dark btn-block">
-            Submit
-          </button>
-          <br />
-
-          <button type="submit" class="btn btn-dark btn-block">
-            Register
-          </button>
+          switch
         </div>
       </div>
     );
--- src/Components/Home/register.js
+++ src/Components/Home/register.js
@@ -1,7 +1,51 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import Select, { components } from "react-select";
+import { ReactComponent as af } from "../images/af.svg";
+import { ReactComponent as nz } from "../images/nz.svg";
+import { ReactComponent as ind } from "../images/in.svg";
+import { ReactComponent as sa } from "../images/sa.svg";
+import { ReactComponent as sl } from "../images/sl.svg";
+import { ReactComponent as pk } from "../images/pk.svg";
+import { ReactComponent as bd } from "../images/bd.svg";
+
+const Options = [
+  { value: "Afghanistan", label: "Afghanistan", icon: <af /> },
+  { value: "New Zealand", label: "New Zealand", icon: <nz /> },
+  { value: "India", label: "India", icon: <ind /> },
+  { value: "South Africa", label: "South Africa", icon: <sa /> },
+  { value: "Sri Lanka", label: "Sri Lanka", icon: <sl /> },
+  { value: "Pakistan", label: "Pakistan", icon: <pk /> },
+  { value: "Bangladesh", label: "Bangladesh", icon: <bd /> }
+];
+const { Option } = components;
+const IconOption = (props) => (
+  <Option {...props}>
+    <img
+      src={props.data.icon}
+      style={{ width: 50 }}
+      // alt={props.data.label}
+    />
+    {props.data.label}
+  </Option>
+);
+
 class register extends React.Component {
   constructor(props) {
     super(props);
+    // this.state={ australia :
+    //   <img src="https://img.icons8.com/color/48/000000/australia-circular.png" />,
+    // india : <img src="https://img.icons8.com/doodle/48/000000/india.png" />,
+    // afghanistan :
+    // <img src="https://img.icons8.com/emoji/48/000000/afghanistan-emoji.png" />,
+    // england :<img src= "https://img.icons8.com/emoji/48/000000/england-emoji.png" />,
+    // newzealand :
+    // <img src= "https://img.icons8.com/emoji/48/000000/new-zealand-emoji.png" />,
+    // srilanka : <img src ="https://img.icons8.com/color/50/000000/sri-lanka.png" />,
+    // southafrica :
+    // <img src= "https://img.icons8.com/emoji/48/000000/south-africa-emoji.png" />,
+    // }
   }
   render() {
     return (
@@ -25,6 +69,37 @@ class register extends React.Component {
               placeholder="name@example.com"
             />
           </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleFormControlInput1"
+              placeholder="Minimum 8 Characters"
+            />
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Confirm Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleFormControlInput1"
+              placeholder="Confirm your Password"
+            />
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Your Favorite Team</label>
+            <div className="opt-text">
+              <Select
+                defaultValue={Options[0]}
+                options={Options}
+                components={{ Option: IconOption }}
+              />
+            </div>
+          </div>
+          <button type="submit" class="btn btn-dark btn-block">
+            Sign In
+          </button>
         </form>
       </div>
     );
