73a22036b4bb34227212a06b2ab1a5c8865df399
Srivatsan Murali <50999780+srivatsan18@users.noreply.github.com>
2021-01-13 01:01:44 +0530

    Register page

---
+++ src/Components/Home/main.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class main extends React.Component {
+  render() {
+    return <div>Home</div>;
+  }
+}
+
+export default main;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import Home from "./Components/home";
+// import Home from "./Components/home";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
