77f4510f56265377bc936b68aa849a10fc29c7b9
abhishek2000123 <43308804+abhishek2000123@users.noreply.github.com>
2021-01-12 19:29:26 +0530

    with image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./styles.css";
 import Home from "./Components/home";
+import Homebg from "./Components/images/homebg.jpg";
 export default function App() {
   return (
     <div className="App">
       <div>
         <home />
       </div>
-      <h2>CricketNow</h2>
       <Home />
+      <img src={Homebg} />
     </div>
   );
 }
--- src/Components/home.js
+++ src/Components/home.js
@@ -3,8 +3,6 @@ class home extends React.Component {
   render() {
     return (
       <div>
-        Crickapp
-        <p>The Next Gen Cricket</p>
         <label></label>
       </div>
     );
